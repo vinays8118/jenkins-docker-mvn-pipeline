fbb6fc8b4124dc78d8a86b2ce63e9b69f366a160
vinays8118 <70208712+vinays8118@users.noreply.github.com>
2020-10-11 00:46:21 +1100

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,4 +6,4 @@ echo "****************************"
 
 WORKSPACE=/Users/giriadini/Documents/Docker-Images/jenkins/jenkins_home/workspace/pipeline1
 
-docker run --rm  -v $WORKSPACE/gitClient:/app -v /Users/vinays8118/dockermount_share/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm  -v $WORKSPACE/gitClient:/app -v /Users/giriadini/dockermount_share/.m2:/root/.m2 -w /app maven:3-alpine "$@"
